52a5b1ef2edb19d2e6a584af08750cf06ddd2bd0
Feldwor <BoQsc@users.noreply.github.com>
2017-05-20 22:53:52 +0300

    Add project via upload from the home old ASUS laptop
    I cannot remember how much this project is done and what part of it is working.

---
+++ Project PATH watch/add_to_path.cmd
@@ -0,0 +1,14 @@
+@ECHO OFF
+SET "currentFolder=%~dp0"
+echo %currentFolder%
+
+
+::LOG path
+
+:Check if path still exists
+::IF EXIST c:\whatever\else SET PATH=%PATH%;c:\whatever\else
+
+
+EXIT
+Check if Environmental Path still exist
+A task that will check if new added folder assigned to Environmental PATH variable still exist.
